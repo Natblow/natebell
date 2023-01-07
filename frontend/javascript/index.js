63c3e1e53f2ab5e1052504656127bbea94bf41d8
Natblow <natblow@hotmail.fr>
2023-01-07 10:33:29 -0500

    initial commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,28 @@
+
+import "index.css"
+import "syntax-highlighting.css"
+
+//   Import all JavaScript & CSS files from src/_components
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
+
+import '@shoelace-style/shoelace/dist/themes/light.css'
+import '@shoelace-style/shoelace/dist/themes/dark.css'
+import '@shoelace-style/shoelace/dist/components/alert/alert.js'
+import '@shoelace-style/shoelace/dist/components/avatar/avatar.js'
+import '@shoelace-style/shoelace/dist/components/breadcrumb/breadcrumb.js'
+import '@shoelace-style/shoelace/dist/components/breadcrumb-item/breadcrumb-item.js'
+import '@shoelace-style/shoelace/dist/components/button/button.js'
+import '@shoelace-style/shoelace/dist/components/card/card.js'
+import '@shoelace-style/shoelace/dist/components/dialog/dialog.js'
+import '@shoelace-style/shoelace/dist/components/divider/divider.js';
+import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js'
+import '@shoelace-style/shoelace/dist/components/icon/icon.js'
+import '@shoelace-style/shoelace/dist/components/input/input.js'
+import '@shoelace-style/shoelace/dist/components/menu/menu.js'
+import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js'
+import '@shoelace-style/shoelace/dist/components/tag/tag.js'
+// import register_icon_library from '@shoelace-style/shoelace/dist/utilities/icon-library.js'
+// import set_base_path from '@shoelace-style/shoelace/dist/utilities/base-path.js'
+// import set_animation from '@shoelace-style/shoelace/dist/utilities/animation-registry.js'
+
+console.info("Bridgetown is loaded!")
